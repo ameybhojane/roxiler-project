bf2977dc8e00aad50e004e5a0dcb51b0e60fa9fe
ameyBhojane <abrock@gmail.com>
2024-02-23 23:19:48 +0530

    dialectModuleUpdated

--- server/database/db.js
+++ server/database/db.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require("sequelize");
+const mysql2 = require('mysql2')
 
 const sequelize = new Sequelize('sql6685510', 'sql6685510', '5Hij3QPwjF', {
     host: 'sql6.freesqldatabase.com',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: mysql2
 });
 
 module.exports = sequelize
